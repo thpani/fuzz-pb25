de0c4427874803aa7a9a71885668e2079c367247
Thomas Pani <thomas.pani@gmail.com>
2025-05-08 09:13:13 +0000

    Initialize chain and get VM

--- pbfuzz.py
+++ pbfuzz.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from eth.abc import VirtualMachineAPI
+from eth.db.atomic import AtomicDB
+from eth.constants import GENESIS_BLOCK_NUMBER, ZERO_ADDRESS
+from eth.chains.base import Chain
+from eth.vm.forks.cancun import CancunVM
+
+def get_vm() -> VirtualMachineAPI:
+    PBFuzzChain = Chain.configure(
+        "PBFuzzChain",
+        vm_configuration=((GENESIS_BLOCK_NUMBER, CancunVM),)
+    )
+    chain = PBFuzzChain.from_genesis(AtomicDB(), {
+        "coinbase": ZERO_ADDRESS,
+        "difficulty": 0,
+        "gas_limit": 10**9,
+        "timestamp": 0,
+    })
+    return chain.get_vm()
+
 def main() -> None:
-    print("Hello, World!")
+    vm = get_vm()
+    print(f"VM: {vm}, nonce: {vm.state.get_nonce(ZERO_ADDRESS)}")
 
 if __name__ == "__main__":
     try:
