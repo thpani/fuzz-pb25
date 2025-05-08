0e8f6b70d936048c5f41854a05e018312f1aac6b
Thomas Pani <thomas.pani@gmail.com>
2025-05-08 10:15:20 +0000

    Add transition creation + execution

--- pbfuzz.py
+++ pbfuzz.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import os
+from typing import cast
 
-from eth.abc import VirtualMachineAPI
+from eth.abc import ComputationAPI, VirtualMachineAPI
 from eth.db.atomic import AtomicDB
 from eth.constants import GENESIS_BLOCK_NUMBER, ZERO_ADDRESS
 from eth.chains.base import Chain
+from eth.vm.forks.shanghai.transactions import ShanghaiTransactionBuilder
 from eth.vm.forks.cancun import CancunVM
 
 from eth_keys.datatypes import PrivateKey
@@ -41,10 +43,49 @@ def get_vm() -> VirtualMachineAPI:
     })
     return chain.get_vm()
 
+
+def create_and_execute_tx(vm: VirtualMachineAPI, signer: Account, to: Address, data: bytes = b'', value: int = 0) -> ComputationAPI:
+    # create a transaction
+    builder = cast(ShanghaiTransactionBuilder, vm.get_transaction_builder())
+    tx = builder.new_unsigned_dynamic_fee_transaction(
+        chain_id=1,
+        nonce=vm.state.get_nonce(signer.address),
+        gas=2_000_000,
+        max_priority_fee_per_gas=1,
+        max_fee_per_gas=875_000_000,
+        to=to,
+        value=value,
+        data=data,
+        access_list=[],
+    )
+
+    # sign the transaction
+    signed_tx = tx.as_signed_transaction(signer.private_key)
+
+    # fund the sender
+    vm.state.set_balance(signer.address, 10**18)
+
+    # apply the transaction
+    _, computation = vm.apply_transaction(vm.get_header(), signed_tx)
+
+    return computation
+
+
 def main() -> None:
     vm = get_vm()
     account = Account()
     print(f"Account: {account}, nonce: {vm.state.get_nonce(account.address)}")
+
+    computation = create_and_execute_tx(
+        vm,
+        signer=account,
+        to=Account().address,
+        data=b'',
+        value=0,
+    )
+    print(f"Account: {account}, nonce: {vm.state.get_nonce(account.address)}")
+    print(f"Computation: {computation}, error: {computation.is_error}, gas: {computation.get_gas_used()}")
+
 
 if __name__ == "__main__":
     try:
